016d06da15d862f670f9d4766d2673f26d6a4deb
Asmahan_Rashed <65909593+Asmahan-Rashed@users.noreply.github.com>
2023-03-26 11:01:15 +0200

    Output

---
+++ output.java
@@ -0,0 +1,13 @@
+public class input {
+    int main(){//block number 1
+
+        boolean x=true,z=true;
+        if(x==true){//block number 2
+
+            if(z==true){//block number 3
+
+                print();
+            }
+        }
+    }
+}
